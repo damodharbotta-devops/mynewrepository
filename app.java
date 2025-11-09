b46a15b624abe6f6a24fabad6f0f5cd6664f2451
root <root@ip-172-31-27-64.ec2.internal>
2025-11-09 11:22:42 +0000

    now i am adding data

--- app.java
+++ app.java
@@ -4,6 +4,7 @@ public class HelloWorld {
     }
 }
 
+<<<<<<< Updated upstream
 # dev-2 push from local
 Main.java
 public class Main {
@@ -11,4 +12,16 @@ public class Main {
     System.out.println("Hello World");
   }
 }
+=======
 
+new code by developer 1 without pull the code
+>>>>>>> Stashed changes
+
+
+
+Main.java
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+  }
+}
