93315a49b205fd0d5ecffa7970827cf3c4f0c978
damodharbotta-devops <bottadamodhar1999@gmail.com>
2025-11-09 16:33:01 +0530

    Update app.java

--- app.java
+++ app.java
@@ -4,5 +4,11 @@ public class HelloWorld {
     }
 }
 
-
+# dev-2 push from local
+Main.java
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+  }
+}
 
